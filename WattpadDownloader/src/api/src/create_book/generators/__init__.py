83b10943bf7bd383a559476b07dd4676fd6f723c
abubakerkhalid3 <160249088+abubakerkhalid3@users.noreply.github.com>
2025-07-05 16:44:00 +0400

    Delete WattpadDownloader directory

--- WattpadDownloader/src/api/src/create_book/generators/__init__.py
+++
@@ -1,4 +0,0 @@
-# ruff: noqa: F401
-
-from .epub import EPUBGenerator
-from .pdf import PDFGenerator
--- WattpadDownloader/src/frontend/svelte.config.js
+++
@@ -1,5 +0,0 @@
-import adapter from '@sveltejs/adapter-static';
-
-const config = { kit: { adapter: adapter({ strict: false }) } };
-
-export default config;
